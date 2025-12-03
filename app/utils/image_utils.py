6393631053f38e621b1f7bd66b36aec95e04a980
samarkandsoul <samarkand.soul.ss@gmail.com>
2025-12-02 21:43:10 -0500

    Update image_utils.py

--- app/utils/image_utils.py
+++ app/utils/image_utils.py
@@ -1,1 +1,21 @@
+"""
+Image Utils – şəkillərlə basic əməliyyatlar üçün skeleton.
+Real funksiyalar Creative Factory-də implement olunacaq.
+"""
 
+from PIL import Image
+from typing import Tuple
+
+
+class ImageUtils:
+    @staticmethod
+    def load_image(path: str) -> Image.Image:
+        return Image.open(path)
+
+    @staticmethod
+    def resize(image: Image.Image, size: Tuple[int, int]) -> Image.Image:
+        return image.resize(size)
+
+    @staticmethod
+    def save(image: Image.Image, path: str) -> None:
+        image.save(path)
