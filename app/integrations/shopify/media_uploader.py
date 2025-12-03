577582780b2817eda49b2613a82c27bf63d75bfa
samarkandsoul <samarkand.soul.ss@gmail.com>
2025-12-02 20:02:28 -0500

    Update media_uploader.py

--- app/integrations/shopify/media_uploader.py
+++ app/integrations/shopify/media_uploader.py
@@ -1,1 +1,18 @@
+from typing import List
+from app.integrations.shopify_client import ShopifyClient
 
+
+class MediaUploader:
+    """
+    Product şəkillərini və media fayllarını Shopify-a yükləyən modul.
+    """
+
+    def __init__(self, client: ShopifyClient) -> None:
+        self.client = client
+
+    def upload_product_images(self, product_id: int, image_urls: List[str]) -> None:
+        """
+        Verilən product üçün şəkil siyahısını yüklə.
+        """
+        # TODO: Shopify image API call-ları
+        raise NotImplementedError("MediaUploader.upload_product_images implement olunmayıb.")
